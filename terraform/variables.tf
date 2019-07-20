0eacd13efa2ab7eebff12b0cf619a8847aac51c4
Ilya Petrusenko <ilyapt4@gmail.com>
2019-07-20 15:45:08 +0300

    split config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,3 +31,13 @@ variable "instance_count" {
   description = "google compute instance count"
   default     = "1"
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
