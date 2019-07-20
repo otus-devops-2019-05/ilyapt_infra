15584179b65fa938e2db6f77860086b50152fa9c
Ilya Petrusenko <ilyapt4@gmail.com>
2019-07-20 10:01:16 +0300

    add ssh keys with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,3 +76,8 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+
+resource "google_compute_project_metadata_item" "appuser" {
+  key = "ssh-keys"
+  value = "appuser:${file(var.public_key_path)}appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+}
