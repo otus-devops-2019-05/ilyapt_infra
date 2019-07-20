ce5c6e6bc834565308a485f02ba9bb6a9be7d6ef
Ilya Petrusenko <ilyapt4@gmail.com>
2019-07-20 16:05:27 +0300

    modules added

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,21 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_project_metadata_item" "appuser" {
-  key = "ssh-keys"
-  value = "appuser:${file(var.public_key_path)}appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+module "app" {
+  source = "modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone = "${var.zone}"
+  app_disk_image = "${var.app_disk_image}"
+}
+
+module "db" {
+  source = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone = "${var.zone}"
+  db_disk_image = "${var.db_disk_image}"
+}
+
+module "vpc" {
+  source = "modules/vpc"
+  source_ranges = ["0.0.0.0/0"]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,3 @@
 output "app_external_ip" {
-  value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
+    value = "${module.app.app_external_ip}"
 }
-#output "cluster_ip" {
-#  value = "${google_compute_global_forwarding_rule.reddit-forward.ip_address}"
-#}
